546619d40c078da2ab06688da2856094aac2708c
Ganesh Girichandiran <gailyganesh93@gmail.com>
2022-07-22 13:45:24 +0200

    Made artifact as binary file

--- BasicOperation.py
+++ BasicOperation.py
@@ -10,3 +10,19 @@ def sub(first, second):
 
 def mul(first,second):
     return first*second
+
+if __name__ == "__main__":
+    print("select the operation:\n 1->Add\n 2->Subration\n 3->Multiplication")
+    operator = input()
+    if(operator=='1'):
+        func=add
+    elif(operator=='2'):
+        func=sub
+    elif(operator=='3'):
+        func=mul
+    else:
+        raise ValueError(f'Supported options are 1, 2 and 3 but you entered {operator}')
+    value1= int(input('Enter the first value: '))
+    value2= int(input('Enter the second value: '))
+    result= func(value1,value2)
+    print(f'Result is: {result}')
